32d9b5b8457a37a754211fec4166e56681fd82d4
MoXuT0 <DenisFisenko2010@yandex.ru>
2022-11-22 16:18:22 +0300

    Исправление опечатки

--- src/App.java
+++ src/App.java
@@ -6,7 +6,7 @@ public class App {
         System.out.println("Домашнее задание");
 
         Book book1 = new Book("Сияние", new Author("Стивен", "Кинг"), 1977);
-        Book book2 = new Book("Дагон", new Author("Говард", "Лавкарфт"), 1917);
+        Book book2 = new Book("Дагон", new Author("Говард", "Лавкрафт"), 1917);
 
         System.out.println(book1.getBookInfo());
         System.out.println(book2.getBookInfo());
